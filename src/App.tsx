24faaa8472f1e96f195d6141807265afe0155163
Brandon Ly <brandonly@lostcoding.com>
2025-03-13 09:28:17 -0500

    refactor into component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { Canvas } from "@react-three/fiber";
 import { useSpring, animated } from "@react-spring/three";
 
 import "./App.css";
-import { Center, Text3D } from "@react-three/drei";
+import { Center, Text3D, type Text3DProps } from "@react-three/drei";
 
 const AnimatedText3D = animated(Text3D);
-function App() {
+
+interface ScaryNumberProps {
+	children: string;
+	position?: Text3DProps["position"];
+}
+function ScaryNumber({ children, position }: ScaryNumberProps) {
 	const [springs, api] = useSpring(() => ({
 		scale: 1,
 		config: (key) => {
@@ -36,41 +41,27 @@ function App() {
 	};
 
 	return (
+		<AnimatedText3D
+			scale={springs.scale}
+			onPointerEnter={handlePointerEnter}
+			onPointerLeave={handlePointerLeave}
+			font="./fonts/Teko_Light_Regular.json"
+			position={position}
+		>
+			{children}
+		</AnimatedText3D>
+	);
+}
+function App() {
+	return (
 		<div id="canvas-container">
 			<Canvas>
 				<ambientLight intensity={0.1} />
 				<Center>
-					<AnimatedText3D
-						scale={springs.scale}
-						onPointerEnter={handlePointerEnter}
-						onPointerLeave={handlePointerLeave}
-						font="./fonts/Teko_Light_Regular.json"
-						position={[0, 0, 0]}
-					>
-						{" "}
-						1
-					</AnimatedText3D>
-					<AnimatedText3D
-						font="./fonts/Teko_Light_Regular.json"
-						position={[1, 1, 0]}
-					>
-						{" "}
-						2
-					</AnimatedText3D>
-					<AnimatedText3D
-						font="./fonts/Teko_Light_Regular.json"
-						position={[2, 2, 0]}
-					>
-						{" "}
-						3
-					</AnimatedText3D>
-					<AnimatedText3D
-						font="./fonts/Teko_Light_Regular.json"
-						position={[3, 3, 0]}
-					>
-						{" "}
-						4
-					</AnimatedText3D>
+					<ScaryNumber position={[0, 0, 0]}>1</ScaryNumber>
+					<ScaryNumber position={[1, 1, 0]}>2</ScaryNumber>
+					<ScaryNumber position={[2, 2, 0]}> 3</ScaryNumber>
+					<ScaryNumber position={[3, 3, 0]}> 4</ScaryNumber>
 				</Center>
 			</Canvas>
 		</div>
