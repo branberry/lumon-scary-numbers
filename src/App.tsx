29f9f715cb2b276985540787ab5f9d95247c1431
Brandon Ly <brandonly@lostcoding.com>
2025-03-13 07:49:10 -0500

    add some numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 
 import "./App.css";
-import { Text3D } from "@react-three/drei";
+import { Center, Text3D } from "@react-three/drei";
 
 function App() {
 	return (
 		<div id="canvas-container">
 			<Canvas>
 				<ambientLight intensity={0.1} />
-				<Text3D font="./fonts/Teko_Light_Regular.json">1 2 3 4</Text3D>
+				<Center>
+					<Text3D font="./fonts/Teko_Light_Regular.json" position={[0, 0, 0]}>
+						{" "}
+						1
+					</Text3D>
+					<Text3D font="./fonts/Teko_Light_Regular.json" position={[1, 1, 0]}>
+						{" "}
+						2
+					</Text3D>
+					<Text3D font="./fonts/Teko_Light_Regular.json" position={[2, 2, 0]}>
+						{" "}
+						3
+					</Text3D>
+					<Text3D font="./fonts/Teko_Light_Regular.json" position={[3, 3, 0]}>
+						{" "}
+						4
+					</Text3D>
+				</Center>
 			</Canvas>
 		</div>
 	);
