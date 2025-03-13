426287751cab71de35fa8ff574540167badde20f
Brandon Ly <brandonly@lostcoding.com>
2025-03-13 07:36:16 -0500

    add 3d font

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+
+import "./App.css";
+import { Text3D } from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div id="canvas-container">
+			<Canvas>
+				<ambientLight intensity={0.1} />
+				<Text3D font="./fonts/Teko_Light_Regular.json">1 2 3 4</Text3D>
+			</Canvas>
+		</div>
+	);
 }
 
-export default App
+export default App;
