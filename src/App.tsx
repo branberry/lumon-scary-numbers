cb6f572eb35aaaecf3faa13c1397f49caeeb48ae
Brandon Ly <brandonly@lostcoding.com>
2025-03-21 23:47:25 -0500

    add pixi text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { Application, extend } from "@pixi/react";
+import { Container, Graphics } from "pixi.js";
+import { useCallback } from "react";
+
 import "./App.css";
 
+extend({
+	Container,
+	Graphics,
+});
+
 function App() {
-	return <div>hello</div>;
+	const drawCallback = useCallback((graphics: Graphics) => {
+		graphics.clear();
+		graphics.setFillStyle({ color: "red" });
+		graphics.rect(0, 0, 100, 100);
+		graphics.fill();
+	}, []);
+
+	return (
+		<div>
+			<Application>
+				<pixiContainer x={100} y={100}>
+					<pixiGraphics draw={drawCallback} />
+					<pixiText />
+				</pixiContainer>
+			</Application>
+		</div>
+	);
 }
 
 export default App;
